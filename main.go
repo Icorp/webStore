eac0c41290dff126cabb587b0078b3b4f4131d7a
Icorp <itteam1602@gmail.com>
2021-02-11 19:08:10 +0600

    rename folder template to web

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("web/*")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{"title": "Home Page"})
 	})
